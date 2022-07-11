82a41c3f10c0be7cd86a1f071ab3b2edca56d504
Alexander Gorlenko <goral92@inbox.ru>
2022-07-11 20:19:40 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="nav__bar">
+      <div class="nav__logo"></div>
+      <nav class="nav__link-list">
+        <router-link to="/" class="nav__link"></router-link>
+      </nav>
+    </div>
+  </header>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import './assets/scss/_variebles.scss';
+@import './assets/scss/_mixins.scss';
+@import './assets/scss/_global.scss';
+</style>
