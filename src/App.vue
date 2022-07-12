b4ba49b0cb5b3c65b88c84abf6323c5e1ec03eb7
Alexander Gorlenko <goral92@inbox.ru>
2022-07-12 11:18:12 +0300

    первичная настройка проекта завершена

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <header>
     <div class="nav__bar">
-      <div class="nav__logo"></div>
+      <div class="nav__logo">logo</div>
       <nav class="nav__link-list">
-        <router-link to="/" class="nav__link"></router-link>
+        <router-link to="/" class="nav__link">Find a Property</router-link>
+        <router-link to="/" class="nav__link">Share Stories</router-link>
+        <router-link to="/" class="nav__link">Rental Guides</router-link>
+        <router-link to="/" class="nav__link">Download Mobile App</router-link>
       </nav>
     </div>
   </header>
@@ -18,4 +21,29 @@ export default {}
 @import './assets/scss/_variebles.scss';
 @import './assets/scss/_mixins.scss';
 @import './assets/scss/_global.scss';
+
+#app {
+  padding: 23px 80px 75px;
+  background-color: rgba(239, 240, 242, 1);
+}
+
+header {
+  width: 100%;
+  .nav__bar {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  .nav__logo {
+    @include titleFont;
+    font-weight: 800;
+    font-size: calcFluidFontSize(24, 35, 320, 1366);
+    line-height: 122%;
+    text-transform: uppercase;
+    margin-right: 133px;
+  }
+  .nav__link {
+    margin-right: 30px;
+  }
+}
 </style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Hello</h1>
+  <main></main>
 </template>
 
 <script>
